fd537b2679510f72513297b4e64335c34c189baa
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2024-10-09 03:21:57 -0300

    fix linters

--- pkg/storage/unified/sql/db/dbimpl/db_test.go
+++ pkg/storage/unified/sql/db/dbimpl/db_test.go
@@ -2,15 +2,12 @@ package dbimpl
 
 import (
 	"database/sql"
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/util/testutil"
 )
-
-var errTest = errors.New("because of reasons")
 
 func TestDB_BeginTx(t *testing.T) {
 	t.Parallel()
